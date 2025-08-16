5ffcfce5e4249dd8eb41e4ffa361ab447c8ebee4
Rajiv C (VWITS) <rajiv.c@volkswagen.co.in>
2025-08-17 00:25:32 +0530

    Prepare Base Model Completed

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 from src.deep_learning_project import logger
 from src.deep_learning_project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.deep_learning_project.pipeline.stage02_prepare_basemodel import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
-
 try:
-    logger.info(f">>>> Starting {STAGE_NAME} <<<<")
-    obj = DataIngestionTrainingPipeline()
-    obj.main()
-    logger.info(f">>>> Completed {STAGE_NAME} <<<<\n\nx========x")
-
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
-    logger.exception(e)
-    raise e
+        logger.exception(e)
+        raise e
+
+
+
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
